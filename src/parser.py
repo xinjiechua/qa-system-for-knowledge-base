e490f0ce5ba2e6589f3c232135726e99e1a80b4d
xinjiechua <xinjie6016@gmail.com>
2025-05-06 00:01:21 +0800

    add parser

---
+++ src/parser.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+from llama_index.core import Document
+from unstructured.partition.pdf import partition_pdf
+from src.embedder import Embedder
+import logging
+
+def parse_pdf(file_path):
+    """
+    Parses a PDF file and returns its content as document.
+    """
+    documents = []
+    logging.info(f"Parsing text from {file_path}")
+
+    for filepath in Path(file_path).iterdir():
+        raw_elements = []
+        raw_elements = partition_pdf(
+            filename=str(filepath),
+            infer_table_structure=True,
+            chunking_strategy="by_title",
+            max_characters=1000,
+            combine_under_n_chars=800,
+            overlap=300,
+            strategy="hi_res",
+            include_metadata=True,
+            multipage_sections=True,
+            extract_images_in_pdf=False,
+        )
+        texts = []
+        for element in raw_elements:
+            if "unstructured.documents.elements.Table" in str(type(element)):
+                texts.append(str(element))
+            elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+                texts.append(str(element))
+            else:
+                texts.append(str(element))
+    
+    filename = os.path.basename(filepath)
+    text_documents = [Document(text=text, metadata={'filename': filename}) for text in texts]
+    for doc in text_documents:
+        doc.embedding = Embedder.generate_embedding(doc.text)
+    documents.extend(text_documents)
+    return documents       
+
+
+
