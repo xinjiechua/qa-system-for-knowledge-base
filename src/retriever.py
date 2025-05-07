5390a04034b3df264e83afb622be0184d0fe01d0
xinjiechua <xinjie6016@gmail.com>
2025-05-07 23:27:20 +0800

    fix bugs in retriever

--- src/retriever.py
+++ src/retriever.py
@@ -1,7 +1,13 @@
+import json
+import logging
 from llama_index.postprocessor.cohere_rerank import CohereRerank
+from llama_index.core.schema import NodeWithScore
 from src.llm import GeminiLLM
 from src.config import Config
-import logging
+from src.vector import VectorDB
+
+llm = GeminiLLM()
+vectordb = VectorDB()
 
 class RAG():
     def __init__(self):
@@ -20,28 +26,44 @@ class RAG():
             latest_message=query
         )
     
-        reformulated_query = GeminiLLM.complete(prompt)
-        logging.debug("Reformulated query: %s", reformulated_query)
-        return reformulated_query.get("reformulated_message", "")
+        reformulated_query = llm.complete(messages=prompt)
+        reformulated_query_dict = json.loads(reformulated_query)
+        print("Reformulated query: %s", reformulated_query)
+        return reformulated_query_dict.get("reformulated_message", "")
 
-    def get_response(self, user_message, history):
+    def get_response(self, user_message, history, selected_course) -> str:
         """
         Retreive relevant chunks from the document and generate a response using the LLM.
         """
         reformulated_query = self.reformulate_query(history=history,query=user_message)
-        retrieved_chunks = self.retriever.retrieve(reformulated_query) 
-        reranked_chunks = self.reranker.postprocess_nodes(retrieved_chunks, query_str=reformulated_query)
-        filtered_chunks = [node for node in reranked_chunks if node.score >= 0.3]
+        retrieved_chunks = vectordb.query(query=reformulated_query, selected_course=selected_course)
+        print("Retrieved chunks:", retrieved_chunks)
         
-        context_str = "\n\n".join([f"Context {i + 1}:\n{content['text']}" for i, content in enumerate(filtered_chunks)])
-        prompt = self.qa_prompt.format(context_str=context_str)
+        if retrieved_chunks.points: 
+            if isinstance(retrieved_chunks, tuple):
+                retrieved_chunks = retrieved_chunks[0]
+            context_str = "\n\n".join([f"Context {i + 1}:\n{content.payload['text']}" for i, content in enumerate(retrieved_chunks)])
+            # nodes = [
+            #     NodeWithScore(id=chunk.id, score=chunk.score, payload=chunk.payload)
+            #     for chunk in retrieved_chunks
+            # ]
+            # reranked_chunks = self.reranker.postprocess_nodes(nodes=nodes, query_str=reformulated_query)
+            # filtered_chunks = [node for node in reranked_chunks if node.score >= 0.3]
+            # print("Filtered chunks: ", filtered_chunks)
+            # context_str = "\n\n".join([f"Context {i + 1}:\n{content.payload['text']}" for i, content in enumerate(filtered_chunks)])
+        else:
+            context_str="No relevant context found."
+            
+        # prompt = self.qa_prompt.format(context_str=context_str)
+        # messages = [{"role": "system", "content": prompt}]
+        # for user_turn, bot_turn in history:
+        #     messages.append({"role": "user", "content": user_turn})
+        #     messages.append({"role": "assistant", "content": bot_turn}) 
+        # messages.append({"role": "user", "content": user_message})
         
-        messages = [{"role": "system", "content": prompt}]
-        for user_turn, bot_turn in history:
-            messages.append({"role": "user", "content": user_turn})
-            messages.append({"role": "assistant", "content": bot_turn}) 
-        messages.append({"role": "user", "content": user_message})
-        
-        response = GeminiLLM.complete_chat(messages)
-        logging.debug("LLM response: %s", response)
-        return response.get("message", "")
+        prompt = self.qa_prompt.format(user_message=user_message, context_str=context_str)
+
+        response = llm.complete(prompt)
+        response_dict = json.loads(response)
+        print("LLM response: %s", response)
+        return response_dict.get("message", "")
